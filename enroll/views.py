c5ef00293b6b39dabddc6a81c5a5aef0ded741b6
Nitesh Singh <ns98738698@gmail.com>
2021-02-26 23:39:47 +0530

    Update views.py

--- enroll/views.py
+++ enroll/views.py
@@ -33,6 +33,22 @@ def delete_data(request,id):
 
 
 
+
+# This Function will Edit
+def update_data(request,id):
+    if request.method == 'POST':
+        pi= User.objects.get(pk=1)
+        fm= StudentRegistration(request.POST, instance=pi)
+        if fm.is_valid():
+            fm.save()
+    else:
+        pi= User.objects.get(pk=1)
+        fm= StudentRegistration(instance=pi)
+    return render(request,'enroll/updatestudent.html',{'form':fm})
+
+
+
+
 # This Function will Edit
 def update_data(request,id):
     if request.method == 'POST':
