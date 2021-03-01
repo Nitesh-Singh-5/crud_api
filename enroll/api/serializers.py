e02f2db0f045c3cfafada8a22d7c11ace7208322
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2021-03-01 22:06:44 +0530

    deleted serializers.py

--- enroll/api/serializers.py
+++ enroll/api/serializers.py
@@ -2,7 +2,3 @@ from enroll.models import User
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'name', 'email', 'password']
